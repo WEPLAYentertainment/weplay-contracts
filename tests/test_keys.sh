ff6aafce7228636966d6b949bee3f739ae67c567
beyondns <beyondnanosecond@gmail.com>
2018-11-04 03:47:34 +0300

    tests

---
+++ tests/test_keys.sh
@@ -0,0 +1,4 @@
+node utils/test_key_enc.js --key tests/keys/k0.json --pwd tests/pwd
+node utils/test_key_enc.js --key tests/keys/k1.json --pwd tests/pwd
+node utils/test_key_enc.js --key tests/keys/k2.json --pwd tests/pwd
+node utils/test_key_enc.js --key tests/keys/k3.json --pwd tests/pwd
